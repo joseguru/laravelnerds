451efeec38a42459d6796e906a640b434c0a1a83
Le-Yo <lenykoskey@yahoo.com>
2014-10-09 00:12:37 +0300

    Initial commit

---
+++ app/models/Nerd.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+/**
+ * nerd
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property integer $nerd_level
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\Nerd whereId($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Nerd whereName($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Nerd whereEmail($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Nerd whereNerdLevel($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Nerd whereCreatedAt($value) 
+ * @method static \Illuminate\Database\Query\Builder|\Nerd whereUpdatedAt($value) 
+ */
+class Nerd extends Eloquent
+    {
+
+    }
