92ef770c16a3e92169b4ec9ed93e5d998071f5a4
Le-Yo <lenykoskey@yahoo.com>
2014-10-09 00:27:07 +0300

    updated the index view to have delete button

--- app/views/nerds/index.blade.php
+++ app/views/nerds/index.blade.php
@@ -48,6 +48,10 @@
 
                 <!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->
                 <!-- we will add this later since its a little more complicated than the other two buttons -->
+				{{ Form::open(array('url' => 'nerds/' . $value->id, 'class' => 'pull-right')) }}
+                    {{ Form::hidden('_method', 'DELETE') }}
+                    {{ Form::submit('Delete this Nerd', array('class' => 'btn btn-warning')) }}
+                {{ Form::close() }}
 
                 <!-- show the nerd (uses the show method found at GET /nerds/{id} -->
                 <a class="btn btn-small btn-success" href="{{ URL::to('nerds/' . $value->id) }}">Show this Nerd</a>
